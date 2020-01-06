02817db6955ae76391cedfc2789513cee0a63c70
Treven Womack <keytonwomack@yahoo.com>
2020-01-06 13:03:21 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,5 +1,9 @@
 def join_ingredients(src)
- 
+  expect(join_ingredients(array_1)).to eq([
+        "I love pepperoni and sausage on my pizza",
+        "I love green olives and green peppers on my pizza",
+        "I love onions and pineapple on my pizza"
+      ])
 end
 
 def find_greater_pair(src)
